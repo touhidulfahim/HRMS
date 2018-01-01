e1193022fbf0678531d9b9dcf8f16580f0c95c9f
touhidulfahim <muhdtohidul@gmail.com>
2018-01-01 07:00:38 +0600

    updatepackage

--- HRMS/Models/Attendance.cs
+++ HRMS/Models/Attendance.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +9,17 @@ namespace HRMS.Models
 {
     public class Attendance
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Display(Name = "Employee")]
         public int EmployeeId { get; set; }
+        [Display(Name = "In Time")]
         public DateTime InTime { get; set; }
+        [Display(Name = "Out Time")]
         public DateTime OutTime { get; set; }
         public string Note { get; set; }
+
+        [ForeignKey("EmployeeId")]
+        public virtual Employee Employee { get; set; }
     }
 }
--- HRMS/Models/Interview.cs
+++ HRMS/Models/Interview.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +9,7 @@ namespace HRMS.Models
 {
     public class Interview
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public int JobId { get; set; }
         public int CandidateId { get; set; }
